779626c15503b6a9970fc47e5a2b3ee50bc4cd85
fatou82 <fatoumatafole169@gmail.com>
2025-12-01 14:22:46 +0000

    Initialisation: Ajout des structures Angular et Spring Boot

---
+++ Backend/src/main/java/com/fatou82/suivi/suivihoraireapi/SuiviHoraireApplication.java
@@ -0,0 +1,13 @@
+package com.fatou82.suivi.suivihoraireapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuiviHoraireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuiviHoraireApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/fatou82/suivi/suivihoraireapi/SuiviHoraireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatou82.suivi.suivihoraireapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuiviHoraireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
